fdf21b8a545392325854998a7b15821523d2ad1c
nile <nile.black@gmail.com>
2010-10-10 00:10:59 +0800

    修复域名问题

--- app/controllers/Auth.java
+++ app/controllers/Auth.java
@@ -1,9 +1,11 @@
 package controllers;
 
 import models.User;
+import play.Play;
 import play.libs.OpenID;
 import play.libs.OpenID.UserInfo;
 import play.mvc.Controller;
+import play.mvc.Http.Request;
 
 public class Auth extends Controller{
 	private static final String KEY_EMAIL = "email";
@@ -12,26 +14,22 @@ public class Auth extends Controller{
 		CloudPaster.index(0);
 	}
 	public static void yahoologin() {
-		if(OpenID.isAuthenticationResponse()) {
-			UserInfo userinfo = OpenID.getVerifiedID();
-			if(userinfo == null) {
-				flash.error("不妙，登陆错误。");
-				login();
-			} else {
-				String email = userinfo.extensions.get(KEY_EMAIL);
-				User user = User.createOrGet(email);
-				session.put(CloudPaster.KEY_USER, user.key);
-				CloudPaster.my(0);
-			}
-		} else {
-			// Verify the id
-			if (!OpenID.id("https://me.yahoo.com").required(KEY_EMAIL, "http://axschema.org/contact/email").verify()) {
-				flash.error("Oops. Cannot contact yahoo");
-				login();
-			}
+		if (!OpenID.id("https://me.yahoo.com").required(KEY_EMAIL, "http://axschema.org/contact/email")
+				.returnTo(Play.configuration.getProperty("auth.returnto", "Auth.login"))
+				.forRealm(Play.configuration.getProperty("auth.returnto", "Auth.login")).verify()) {
+			flash.error("Oops. Cannot contact yahoo");
+			login();
 		}
 	}
 	public static void googlelogin() {
+		if (!OpenID.id("https://www.google.com/accounts/o8/id").required(KEY_EMAIL, "http://axschema.org/contact/email")
+				.returnTo(Play.configuration.getProperty("auth.returnto", "Auth.login"))
+				.forRealm(Play.configuration.getProperty("auth.returnto", "Auth.login")).verify()) {
+			flash.error("Oops. Cannot contact google");
+			CloudPaster.index(0);
+		}
+	}
+	public static void login() {
 		if(OpenID.isAuthenticationResponse()) {
 	        // Retrieve the verified id
 	        UserInfo userinfo = OpenID.getVerifiedID();
@@ -44,15 +42,7 @@ public class Auth extends Controller{
               session.put(CloudPaster.KEY_USER, user.key);
               CloudPaster.my(0);
 	        }
-		} else {
-			// Verify the id
-			if (!OpenID.id("https://www.google.com/accounts/o8/id").required(KEY_EMAIL, "http://axschema.org/contact/email").verify()) {
-				flash.error("Oops. Cannot contact google");
-				CloudPaster.index(0);
-			}
 		}
-	}
-	public static void login() {
 		render();
 	}
 }
