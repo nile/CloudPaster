4054084556f1bd355ea9d9498bd4e91ccc7275bd
nile <nile.black@gmail.com>
2010-10-29 15:24:27 +0800

    add mooeditable chinese lanuage

---
+++ public/javascripts/editor/Localization/MooEditable.Chinese.CN.js
@@ -0,0 +1,116 @@
+/*
+---
+ 
+script: MooEditable.English.US.js
+ 
+description: MooEditable messages for US English.
+ 
+license: MIT-style license
+ 
+authors:
+- Lim Chee Aun
+ 
+requires:
+- MooEditable
+- more:1.2.4.2/Lang
+
+provides: [MooEditable.English.US]
+
+...
+*/
+
+MooTools.lang.set('zh-CN', 'MooEditable', {
+	
+	// MooEditable
+	ok: '确定',
+	cancel: '取消',
+	bold: '加粗',
+	italic: '倾斜',
+	underline: '下划线',
+	strikethrough: '删除线',
+	unorderedList: '无序列表',
+	orderedList: '1 2 ... ',
+	indent: '增加缩进',
+	outdent: '减少缩进',
+	undo: '撤销',
+	redo: '重做',
+	removeHyperlink: '取消链接',
+	addHyperlink: '添加链接',
+	selectTextHyperlink: '请选择需要添加链接的文字',
+	enterURL: '地址',
+	enterImageURL: '图片地址',
+	addImage: '插入图片',
+	toggleView: '源码',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: 'Insert custom character',
+	insertCharacter: 'Insert character',
+	
+	// MooEditable.Extras
+	blockFormatting: 'Block Formatting',
+	paragraph: 'Paragraph',
+	heading1: 'Heading 1',
+	heading2: 'Heading 2',
+	heading3: 'Heading 3',
+	alignLeft: 'Align Left',
+	alignRight: 'Align Right',
+	alignCenter: 'Align Center',
+	alignJustify: 'Align Justify',
+	removeFormatting: 'Remove Formatting',
+	insertHorizontalRule: 'Insert Horizontal Rule',
+	
+	// MooEditable.Flash
+	embed: 'Enter embed code',
+	flashEmbed: 'Flash Embed',
+	
+	// MooEditable.Forecolor
+	changeColor: 'Change Color',
+	
+	// MooEditable.Image
+	imageAlt: 'alt',
+	imageClass: 'class',
+	imageAlign: 'align',
+	imageAlignNone: 'none',
+	imageAlignLeft: 'left',
+	imageAlignCenter: 'center',
+	imageAlignRight: 'right',
+	addEditImage: 'Add/Edit Image',
+	
+	// MooEditable.Pagebreak
+	pageBreak: 'Page break',
+	
+	// MooEditable.Smiley
+	insertSmiley: 'Insert Smiley',
+	
+	// MooEditable.Table
+	tableColumns: 'columns',
+	tableRows: 'rows',
+	tableWidth: 'width',
+	tableClass: 'class',
+	tableType: 'type',
+	tableHeader: 'Header',
+	tableCell: 'Cell',
+	tableAlign: 'align',
+	tableAlignNone: 'none',
+	tableAlignCenter: 'center',
+	tableAlignRight: 'right',
+	tableValign: 'vertical align',
+	tableValignNone: 'none',
+	tableValignTop: 'top',
+	tableValignMiddle: 'middle',
+	tableValignBottom: 'bottom',
+	addTable: 'Add Table',
+	editTable: 'Edit Table',
+	addTableRow: 'Add Table Row',
+	editTableRow: 'Edit Table Row',
+	mergeTableRow: 'Merge Table Row',
+	splitTableRow: 'Split Table Row',
+	deleteTableRow: 'Delete Table Row',
+	addTableCol: 'Add Table Column',
+	editTableCol: 'Edit Table Column',
+	mergeTableCell: 'Merge Table Cell',
+	splitTableCell: 'Split Table Cell',
+	deleteTableCol: 'Delete Table Column'
+	
+});
+MooTools.lang.setLanguage('zh-CN');
