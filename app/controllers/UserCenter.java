993d0220f07d3dab2bebb56b5053fcfc5f9f0d39
unknown <nile@.(none)>
2011-03-31 14:30:35 +0800

    git fix me error

--- app/controllers/UserCenter.java
+++ app/controllers/UserCenter.java
@@ -1,36 +1,40 @@
 package controllers;
 
-import java.util.List;
+import controllers.deadbolt.Deadbolt;
+import controllers.deadbolt.Restrict;
+import controllers.deadbolt.Restrictions;
 import models.Subscribe;
 import models.User;
-import play.db.jpa.GenericModel.JPAQuery;
 import play.mvc.Before;
 import play.mvc.Controller;
+import play.mvc.With;
 
+import java.util.List;
+@With({Deadbolt.class,GlobalUser.class})
 public class UserCenter extends Controller{
-	@Before
-	static void startTimer() {
-		User user = Auth.getLoginUser();
-		renderArgs.put("user", user);
-	}
+    @Restrictions(@Restrict("user"))
 	static public void save(User user) {
 		user.save();
 		index(Auth.getLoginUser().id);
 	}
+    @Restrictions(@Restrict("user"))
 	static public void edit() {
 		User user = Auth.getLoginUser();
 		render(user);
 	}
+    @Restrictions(@Restrict("user"))
 	static public void index(Long id) {
 		User user = Auth.getLoginUser();
                 List<String> subscribes = Subscribe.find("select topic from Subscribe where user = ?", user).fetch();
 		render(user,subscribes);
 	}
+    @Restrictions(@Restrict("user"))
         static public void unsubscribe(String topic){
             final User user = Auth.getLoginUser();
             Subscribe.unsubscribe(user,topic);
             //index(user.id);
         }
+    @Restrictions(@Restrict("user"))
         static public void subscribe(String topic){
             final User user = Auth.getLoginUser();
             Subscribe.subscribe(user,topic);
