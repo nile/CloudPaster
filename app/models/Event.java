a2627c61c3a26ac9182f840beac306f1fcc45ec3
nile black <nile.black@qq.com>
2012-10-18 23:48:49 +0800

    修改linux表名大小写的问题

--- app/models/Event.java
+++ app/models/Event.java
@@ -3,11 +3,13 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import models.Paster;
 import models.User;
 import play.db.jpa.Model;
 @Entity
+@Table(name="event")
 public class Event extends Model{
 	@OneToOne
 	public User user;
